07bcbe02088c4fedeabc612653cd7bc37acbaa80
Ayaansiddiq07 <ayaansiddia@gamil.com>
2025-11-16 20:24:57 +0530

    WEbsite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+// For environment variables
+interface ImportMetaEnv {
+  readonly VITE_EMAILJS_SERVICE_ID: string;
+  readonly VITE_EMAILJS_TEMPLATE_ID: string;
+  readonly VITE_EMAILJS_PUBLIC_KEY: string;
+  readonly VITE_CONTACT_EMAIL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
